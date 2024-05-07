8e8634d7c4098930d498acfa453cc4a39d200acd
Mohit Arora <me@aboutmohit.com>
2024-05-07 10:07:47 -0400

    Initial Commit

---
+++ LearnFlags/LearnFlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnFlagsApp.swift
+//  LearnFlags
+//
+//  Created by Mohit Arora on 2024-05-07.
+//
+
+import SwiftUI
+
+@main
+struct LearnFlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
